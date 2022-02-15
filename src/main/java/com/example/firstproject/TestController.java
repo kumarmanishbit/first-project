d135b983db030ea43d4c371577a208cb2d9de0f1
Manish Kumar <manish.br.kumar@oracle.com>
2022-02-15 12:21:15 +0530

    First commit

---
+++ src/main/java/com/example/firstproject/TestController.java
@@ -0,0 +1,13 @@
+package com.example.firstproject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String getHello() {
+        return "Hello There!!!";
+    }
+}
